62b9cb6b190281670953ee0173a8b4054f908d6f
Ivan Sklyarenko <wanya.workmail@gmail.com>
2020-11-12 20:14:37 +0700

    Add access for model Contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,19 @@ class ContactsController < ApplicationController
 
   def create
     #render plain: params[:contact].inspect - Это нужно для проверки, ну, думаю, что этои так понятно
+  @contact = Contact.new(contact_params)
+
+  if @contact.valid?
+    @contact.save
+  else
+    render action: @contact
+  end
+
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:title, :message)
   end
 end
